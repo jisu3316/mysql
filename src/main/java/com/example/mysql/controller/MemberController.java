2a2c8ef4787703086980ac4ae9d8cf10143e38ff
kimjisu <jisu3316@naver.com>
2022-12-14 22:56:08 +0900

    회원 정보 조회 구현

--- src/main/java/com/example/mysql/controller/MemberController.java
+++ src/main/java/com/example/mysql/controller/MemberController.java
@@ -1,21 +1,27 @@
 package com.example.mysql.controller;
 
+import com.example.mysql.domain.member.dto.MemberDto;
 import com.example.mysql.domain.member.dto.RegisterMemberCommand;
-import com.example.mysql.domain.member.entity.Member;
+import com.example.mysql.domain.member.service.MemberReadService;
 import com.example.mysql.domain.member.service.MemberWriteService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberWriteService memberWriteService;
+    private final MemberReadService memberReadService;
 
     @PostMapping("/members")
-    public Member register(@RequestBody RegisterMemberCommand command) {
-        return memberWriteService.create(command);
+    public MemberDto register(@RequestBody RegisterMemberCommand command) {
+         var member = memberWriteService.register(command);
+        return memberReadService.toDto(member);
+    }
+
+    @GetMapping("/members/{id}")
+    public MemberDto getMember(@PathVariable Long id) {
+        return memberReadService.getMember(id);
     }
 }
---
+++ src/main/java/com/example/mysql/domain/member/service/MemberReadService.java
@@ -0,0 +1,23 @@
+package com.example.mysql.domain.member.service;
+
+import com.example.mysql.domain.member.dto.MemberDto;
+import com.example.mysql.domain.member.entity.Member;
+import com.example.mysql.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberReadService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberDto getMember(Long id) {
+        var member = memberRepository.findById(id).orElseThrow();
+        return toDto(member);
+    }
+
+    public MemberDto toDto(Member member) {
+        return new MemberDto(member.getId(), member.getEmail(), member.getNickname(), member.getBirthday());
+    }
+}
